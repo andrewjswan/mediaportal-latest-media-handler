f1deeb2b0cd48311b1e356666f8ccb36238f5c15
yoavain <yoavain@4ba469b5-f7f6-22fa-9553-b7ed773beacf>
2014-10-27 07:46:36 +0000

    Added scripts etc.

---
+++ scripts/Deploy.bat
@@ -0,0 +1,16 @@
+@echo off
+cls
+Title Deploying MediaPortal Latest Media Handler (RELEASE)
+cd ..
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+copy /y "LatestMediaHandler\bin\Release\LatestMediaHandler.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\process\"
+
+cd scripts
---
+++ scripts/Deploy_Debug.bat
@@ -0,0 +1,15 @@
+@echo off
+cls
+Title Deploying MediaPortal Latest Media Handler (DEBUG)
+cd ..
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+copy /y "LatestMediaHandler\bin\Debug\LatestMediaHandler.dll" "%PROGS%\Team MediaPortal\MediaPortal\plugins\process\"
+cd scripts
