f04d4325b04d36c469825e1a78a5c7e02567d9a6
yoavain <yoavain@gmail.com>
2015-03-25 23:27:27 +0200

    GIT & SVN scripts

---
+++ scripts/Build_Debug_GIT.bat
@@ -0,0 +1,30 @@
+@echo off
+cls
+Title Building MediaPortal Latest Media Handler (RELEASE)
+cd ..
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+	:: 64-bit
+	set PROGS=%programfiles(x86)%
+	goto CONT
+:32BIT
+	set PROGS=%ProgramFiles%	
+:CONT
+
+setlocal enabledelayedexpansion
+
+:: Prepare version
+for /f "tokens=*" %%a in ('git rev-list HEAD --count') do set REVISION=%%a 
+set REVISION=%REVISION: =%
+"scripts\Tools\sed.exe" -i "s/\$WCREV\$/%REVISION%/g" LatestMediaHandler\Properties\AssemblyInfo.cs
+
+:: Build
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=DEBUG /fl /flp:logfile=LatestMediaHandler.log;verbosity=diagnostic LatestMediaHandler.sln
+
+:: Revert version
+git checkout LatestMediaHandler\Properties\AssemblyInfo.cs
+
+cd scripts
+
+pause
+
--- scripts/Build_Debug_SVN.bat
+++ scripts/Build_Debug_SVN.bat
@@ -11,8 +11,14 @@ if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
 	set PROGS=%ProgramFiles%	
 :CONT
 
+:: Prepare version
+subwcrev . LatestMediaHandler\Properties\AssemblyInfo.cs LatestMediaHandler\Properties\AssemblyInfo.cs
+
 :: Build
 "%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBUILD.exe" /target:Rebuild /property:Configuration=DEBUG /fl /flp:logfile=LatestMediaHandler.log;verbosity=diagnostic LatestMediaHandler.sln
+
+:: Revert version
+svn revert LatestMediaHandler\Properties\AssemblyInfo.cs
 
 cd scripts
 
