730004071f465bdf5a0fc7e2a4732178737a89b3
Andrew J.Swan <ajs.blackhole@gmail.com>
2024-03-27 17:13:25 +0200

    Update Create Installer

--- scripts/Create_Installer.bat
+++ scripts/Create_Installer.bat
@@ -15,5 +15,11 @@ IF NOT EXIST "%PROGS%\Team MediaPortal\MediaPortal\" SET PROGS=C:
 :: Get version from DLL
 FOR /F "tokens=*" %%i IN ('Tools\sigcheck.exe /accepteula /nobanner /n "..\LatestMediaHandler\bin\Release\LatestMediaHandler.dll"') DO (SET version=%%i)
 
+:: Temp xmp2 file
+copy /Y LatestMediaHandler.xmp2 LatestMediaHandlerTemp.xmp2
+
 :: Build mpe1
-"%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" LatestMediaHandler.xmp2 /B /V=%version% /UpdateXML
+"%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" LatestMediaHandlerTemp.xmp2 /B /V=%version% /UpdateXML
+
+:: Cleanup
+del LatestMediaHandlerTemp.xmp2
